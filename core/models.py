f1a233cf34665592e7a2941b3e89621bd1b51ee8
Ali Parvizi <snparvizi75@gmail.com>
2019-08-08 16:51:29 +0430

    city model, id info user

--- core/models.py
+++ core/models.py
@@ -3,11 +3,32 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+class City(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class UserProfile(models.Model):
     user = models.ForeignKey(User, related_name="user_profile", on_delete=models.CASCADE)
     account_number = models.CharField(max_length=255, blank=True, null=True)
     shaba_number = models.CharField(max_length=255, blank=True, null=True)
 
+    name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    father_name = models.CharField(max_length=255, blank=True, null=True)
+    meli_number = models.CharField(max_length=255, blank=True, null=True)
+    sh_number = models.CharField(max_length=255, blank=True, null=True)
+
+    mobile_number = models.CharField(max_length=255, blank=True, null=True)
+    
+    land_number = models.CharField(max_length=255, blank=True, null=True)
+    postal_code = models.CharField(max_length=255, blank=True, null=True)
+    city = models.ForeignKey(City, related_name="user_profile", on_delete=models.CASCADE, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+
+    is_verified = models.BooleanField(default=False)
+    
 class Gate(models.Model):
     profile = models.ForeignKey(UserProfile, related_name="gates", on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
