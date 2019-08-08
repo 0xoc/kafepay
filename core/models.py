ab757c959364557a8508dbe24fb234873591bf4a
Ali Parvizi <snparvizi75@gmail.com>
2019-08-08 16:52:28 +0430

    __str__

--- core/models.py
+++ core/models.py
@@ -28,13 +28,18 @@ class UserProfile(models.Model):
     address = models.CharField(max_length=255, blank=True, null=True)
 
     is_verified = models.BooleanField(default=False)
-    
+
+    def __str__(self):
+        return str(self.user)
+
 class Gate(models.Model):
     profile = models.ForeignKey(UserProfile, related_name="gates", on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=1000)
     url = models.CharField(max_length=255)
 
+    def __str__(self):
+        return str(self.title)
 
 class Product(models.Model):
     profile = models.ForeignKey(UserProfile, related_name="products", on_delete=models.CASCADE)
@@ -42,6 +47,9 @@ class Product(models.Model):
     description = models.TextField()
     price = models.IntegerField()
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+
+    def __str__(self):
+        return str(self.title)
 
 
 
